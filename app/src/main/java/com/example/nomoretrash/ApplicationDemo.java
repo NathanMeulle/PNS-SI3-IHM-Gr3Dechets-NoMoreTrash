18e5cde79fe6c269ce1adccb91ee4e2fc3ccda85
NathanMeulle <54913885+NathanMeulle@users.noreply.github.com>
2020-04-01 15:55:32 +0200

    Création channel Notification #1

---
+++ app/src/main/java/com/example/nomoretrash/ApplicationDemo.java
@@ -0,0 +1,36 @@
+package com.example.nomoretrash;
+
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+import java.util.Objects;
+
+public class ApplicationDemo extends Application {
+    public static final String CHANNEL_ID = "channelNoMoreTrash";
+    private static NotificationManager notificationManager;
+
+
+    private void createNotificationChannel(CharSequence name, String descriptionChannel, int importance) {
+        // for API 26+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(descriptionChannel);
+            // upadate NotificationManager - cannot be changed after
+            notificationManager = getSystemService(NotificationManager.class);
+            Objects.requireNonNull(notificationManager).createNotificationChannel(channel);
+        }
+    }
+
+
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        createNotificationChannel(  "channel NoMoreTrash",
+                "Channel pour l'application NoMoreTrash",
+                NotificationManager.IMPORTANCE_DEFAULT );
+    }
+}
