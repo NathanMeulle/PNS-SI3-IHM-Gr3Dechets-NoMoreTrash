a8bc10fe8c72a4dcc4039d8a0532691ab42c43a8
LeoBurette <54930754+LeoBurette@users.noreply.github.com>
2020-04-04 15:46:45 +0200

    ajout bouton

--- app/src/main/java/com/example/nomoretrash/MainActivity.java
+++ app/src/main/java/com/example/nomoretrash/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.nomoretrash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.icu.text.CaseMap;
 import android.preference.PreferenceManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.nomoretrash.map.MainMapActivity;
 
 import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
@@ -25,5 +30,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button button = findViewById(R.id.boutonMap);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainMapActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
+
