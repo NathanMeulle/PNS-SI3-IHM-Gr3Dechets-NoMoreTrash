edbe5433430184e3dc685b7d7a18943f14e3f174
LeoBurette <54930754+LeoBurette@users.noreply.github.com>
2020-04-04 15:31:55 +0200

    ajout map activity

--- app/src/main/java/com/example/nomoretrash/MainActivity.java
+++ app/src/main/java/com/example/nomoretrash/MainActivity.java
@@ -2,7 +2,22 @@ package com.example.nomoretrash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
+import android.icu.text.CaseMap;
+import android.preference.PreferenceManager;
+
 import android.os.Bundle;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
+import org.osmdroid.views.overlay.OverlayItem;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/nomoretrash/map/MainMapActivity.java
@@ -0,0 +1,76 @@
+package com.example.nomoretrash.map;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+
+import com.example.nomoretrash.R;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
+import org.osmdroid.views.overlay.OverlayItem;
+
+import java.util.ArrayList;
+
+public class MainMapActivity extends AppCompatActivity {
+    private MapView map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        IMapController mapController;
+
+        super.onCreate(savedInstanceState);
+        Configuration.getInstance().load(   getApplicationContext(), PreferenceManager.getDefaultSharedPreferences(getApplicationContext()) );
+        setContentView(R.layout.activity_main);
+
+        map = findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+
+        mapController = map.getController();
+        mapController.setZoom(18.0);
+        GeoPoint startPoint = new GeoPoint(43.181866, 5.703372);
+        mapController.setCenter(startPoint);
+
+        ArrayList<OverlayItem> items = new ArrayList<OverlayItem>();
+        OverlayItem home = new OverlayItem("Maison de Léo","C'est là ou ses parents habitent", new GeoPoint(43.189122, 5.704391));
+        Drawable m = home.getMarker(0);
+
+        items.add(home);
+        ItemizedOverlayWithFocus<OverlayItem> mOverlay = new ItemizedOverlayWithFocus<OverlayItem>(this, items,
+                new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
+                    @Override
+                    public boolean onItemSingleTapUp(int index, OverlayItem item) {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onItemLongPress(int index, OverlayItem item) {
+                        return false;
+                    }
+                });
+
+        mOverlay.setFocusItemsOnTap(true);
+        map.getOverlays().add(mOverlay);
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        map.onResume();
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        map.onPause();
+    }
+}
