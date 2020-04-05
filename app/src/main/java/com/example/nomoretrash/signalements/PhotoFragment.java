678efeb351d4fe61da96d3f1f81ece588659a076
RomainLamere <54950366+RomainLamere@users.noreply.github.com>
2020-04-05 17:35:51 +0200

    ajout des fragments

---
+++ app/src/main/java/com/example/nomoretrash/signalements/PhotoFragment.java
@@ -0,0 +1,23 @@
+package com.example.nomoretrash.signalements;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.nomoretrash.R;
+
+public class PhotoFragment extends Fragment {
+    public PhotoFragment() {//vide
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.photo_fragment, container, false);
+
+
+        return view;
+    }
+}
