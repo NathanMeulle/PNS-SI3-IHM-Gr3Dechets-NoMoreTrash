e3d7ee1aa0aa22bcbd283a2626a6d3491da28d85
RomainLamere <54950366+RomainLamere@users.noreply.github.com>
2020-04-05 14:48:52 +0200

    je fais semblant de participer

---
+++ app/src/main/java/com/example/nomoretrash/signalements/DescriptionFragment.java
@@ -0,0 +1,25 @@
+package com.example.nomoretrash.signalements;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.nomoretrash.R;
+
+public class DescriptionFragment extends Fragment {
+    public DescriptionFragment() {//vide
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.description_fragment, container, false);
+
+
+        return view;
+    }
+
+
+}
