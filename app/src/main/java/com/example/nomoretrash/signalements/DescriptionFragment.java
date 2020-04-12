893448fa39cbb9c164c0b39c47dbaeb88589edbf
RomainLamere <54950366+RomainLamere@users.noreply.github.com>
2020-04-12 15:04:53 +0200

    Reglage des cas imossible pour les check box

--- app/src/main/java/com/example/nomoretrash/signalements/DescriptionFragment.java
+++ app/src/main/java/com/example/nomoretrash/signalements/DescriptionFragment.java
@@ -61,26 +61,25 @@ public class DescriptionFragment extends Fragment {
 
 
     private void checkBoxCreation(View rootView) {
-        CheckBox checkBox_dechet_unique = rootView.findViewById(R.id.checkbox_dechet_unique);
-        CheckBox checkbox_decharge_sauvage = rootView.findViewById(R.id.checkbox_decharge_sauvage);
+        final CheckBox checkBox_dechet_unique = rootView.findViewById(R.id.checkbox_dechet_unique);
+        final CheckBox checkbox_decharge_sauvage = rootView.findViewById(R.id.checkbox_decharge_sauvage);
         CheckBox checkbox_verre = rootView.findViewById(R.id.checkbox_verre);
         CheckBox checkbox_carton = rootView.findViewById(R.id.checkbox_carton);
         CheckBox checkbox_papier = rootView.findViewById(R.id.checkbox_papier);
         CheckBox checkbox_plastique = rootView.findViewById(R.id.checkbox_plastique);
         CheckBox checkbox_metal = rootView.findViewById(R.id.checkbox_metal);
         CheckBox checkbox_autre = rootView.findViewById(R.id.checkbox_autre);
-        CheckBox checkbox_petit = rootView.findViewById(R.id.checkbox_petit);
-        CheckBox checkbox_gros = rootView.findViewById(R.id.checkbox_gros);
+        final CheckBox checkbox_petit = rootView.findViewById(R.id.checkbox_petit);
+        final CheckBox checkbox_gros = rootView.findViewById(R.id.checkbox_gros);
 
 
         checkBox_dechet_unique.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!DECHET_UNIQUE)
-                    DECHET_UNIQUE = true;
-                else
-                    DECHET_UNIQUE = false;
-
+                DECHET_UNIQUE = !DECHET_UNIQUE;
+                if (DECHET_UNIQUE && DECHARGE_SAUVAGE)
+                    DECHARGE_SAUVAGE = false;
+                    checkbox_decharge_sauvage.setChecked(false);
                 Log.d("DECHET_UNIQUE", DECHET_UNIQUE + "");
             }
         });
@@ -89,7 +88,9 @@ public class DescriptionFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 DECHARGE_SAUVAGE = !DECHARGE_SAUVAGE;
-
+                if (DECHET_UNIQUE && DECHARGE_SAUVAGE)
+                    DECHET_UNIQUE = false;
+                    checkBox_dechet_unique.setChecked(false);
                 Log.d("DECHARGE_SAUVAGE", DECHARGE_SAUVAGE + "");
             }
         });
@@ -152,7 +153,9 @@ public class DescriptionFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 GROS = !GROS;
-
+                if (GROS && PETIT)
+                    PETIT = false;
+                    checkbox_petit.setChecked(false);
                 Log.d("GROS", GROS + "");
             }
         });
@@ -162,15 +165,73 @@ public class DescriptionFragment extends Fragment {
 
             public void onClick(View view) {
                 PETIT = !PETIT;
-
-
+                if (GROS && PETIT)
+                    GROS = false;
+                    checkbox_gros.setChecked(false);
                 Log.d("PETIT", PETIT + "");
             }
         });
+    }
+    public static void reinitialiseCheckbox(){
+        setAUTRE(false);
+        setCARTON(false);
+        setDechargeSauvage(false);
+        setDechetUnique(false);
+        setGROS(false);
+        setMETAL(false);
+        setPAPIER(false);
+        setPETIT(false);
+        setPLASTIQUE(false);
+        setVERRE(false);
+    }
+    /*
+    ########################### SETTERS ###########################
+     */
+
+    public static void setDechetUnique(boolean dechetUnique) {
+        DECHET_UNIQUE = dechetUnique;
+    }
+
+    public static void setDechargeSauvage(boolean dechargeSauvage) {
+        DECHARGE_SAUVAGE = dechargeSauvage;
+    }
+
+    public static void setVERRE(boolean VERRE) {
+        DescriptionFragment.VERRE = VERRE;
+    }
+
+    public static void setCARTON(boolean CARTON) {
+        DescriptionFragment.CARTON = CARTON;
+    }
+
+    public static void setPAPIER(boolean PAPIER) {
+        DescriptionFragment.PAPIER = PAPIER;
+    }
+
+    public static void setPLASTIQUE(boolean PLASTIQUE) {
+        DescriptionFragment.PLASTIQUE = PLASTIQUE;
+    }
+
+    public static void setMETAL(boolean METAL) {
+        DescriptionFragment.METAL = METAL;
+    }
+
+    public static void setAUTRE(boolean AUTRE) {
+        DescriptionFragment.AUTRE = AUTRE;
+    }
+
+    public static void setGROS(boolean GROS) {
+        DescriptionFragment.GROS = GROS;
+    }
+
+    public static void setPETIT(boolean PETIT) {
+        DescriptionFragment.PETIT = PETIT;
+    }
+
     /*
     ########################### GETTERS ###########################
      */
-    }
+
     public static boolean isDechetUnique() {
         return DECHET_UNIQUE;
     }
