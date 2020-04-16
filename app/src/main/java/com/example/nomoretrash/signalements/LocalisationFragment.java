ff13776c218ccc9c712a04540feb9838a7e69578
NathanMeulle <54913885+NathanMeulle@users.noreply.github.com>
2020-04-16 22:16:51 +0200

    Permission localisation

--- app/src/main/java/com/example/nomoretrash/signalements/LocalisationFragment.java
+++ app/src/main/java/com/example/nomoretrash/signalements/LocalisationFragment.java
@@ -1,30 +1,63 @@
 package com.example.nomoretrash.signalements;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.nomoretrash.R;
 
 import org.osmdroid.api.IMapController;
+import org.osmdroid.events.MapListener;
+import org.osmdroid.events.ScrollEvent;
+import org.osmdroid.events.ZoomEvent;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.ItemizedIconOverlay;
 import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
 import org.osmdroid.views.overlay.OverlayItem;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class LocalisationFragment extends Fragment {
 
     private MapView map;
+    private SignalementObject signalementObject;
+    ImageButton boutonMaPosition;
+    private static final int PERMISSION_CODE = 1000;
+    private int etat;
+    LocationListener ecouteurGPS;
+    LocationManager locationManager = null;
+    private String fournisseur;
 
-    public LocalisationFragment() {//vide
+
+
+    public LocalisationFragment() {
+        //on récupère l'objet signalement
+        this.signalementObject = DescriptionFragment.getSignalementObject();
     }
 
     public static LocalisationFragment newInstance() {
@@ -35,7 +68,7 @@ public class LocalisationFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.localisation_fragment, container, false);
-        IMapController mapController;
+        final IMapController mapController;
 
         map = rootView.findViewById(R.id.map);
         map.setTileSource(TileSourceFactory.MAPNIK);
@@ -67,6 +100,23 @@ public class LocalisationFragment extends Fragment {
 
         mOverlay.setFocusItemsOnTap(true);
         map.getOverlays().add(mOverlay);
+        boutonMaPosition = rootView.findViewById(R.id.boutonMaPosition);
+        boutonMaPosition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (ContextCompat.checkSelfPermission(LocalisationFragment.this.getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_DENIED) {
+                        //Permission non accordée, on demande de nouveau la permission
+                        String[] permission = {Manifest.permission.ACCESS_FINE_LOCATION};
+                        //POP UP
+                        requestPermissions(permission, PERMISSION_CODE);
+                    } else {
+                        //permission ok
+                    }
+                    //recentrateMap(mapController);
+                }
+            }
+        });
 
         return rootView;
     }
