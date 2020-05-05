3614ed39003a948834d9198dbc005ed5fb1b851b
RomainLamere <54950366+RomainLamere@users.noreply.github.com>
2020-05-05 15:53:59 +0200

    debut de statistique

--- app/src/main/java/com/example/nomoretrash/statistiques/StatistiquesActivity.java
+++ app/src/main/java/com/example/nomoretrash/statistiques/StatistiquesActivity.java
@@ -14,6 +14,18 @@ import java.util.HashMap;
 
 public class StatistiquesActivity extends AppCompatActivity {
 
+    private int nbVerre;
+    private int nbCarton;
+    private int nbPapier;
+    private int nbPlastique;
+    private int nbMetal;
+    private int nbAutre;
+
+    private static final int Baton = 1;
+    private static final int Circulaire = 2;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,9 +43,63 @@ public class StatistiquesActivity extends AppCompatActivity {
         Intent intent = getIntent();
         if (intent.hasExtra("ma_liste_de_signalements")) {
             ArrayList<String> res = intent.getStringArrayListExtra("ma_liste_de_signalements");
+            HashMap hashmap = setStat(res);
+            try {
+                createDiagram(1,hashmap);
+            } catch (Throwable throwable) {
+                throwable.printStackTrace();
+            }
         }
 
 
     }
 
+    private HashMap setStat(ArrayList<String> res) {
+        nbVerre = 0;
+        nbCarton = 0;
+        nbPapier = 0;
+        nbPlastique = 0;
+        nbMetal = 0;
+        nbAutre = 0;
+        HashMap hashMap = new HashMap();
+
+        for (String str : res) {
+            if (str.contains("verre")) {
+                nbVerre++;
+            }
+            if (str.contains("carton")) {
+                nbCarton++;
+            }
+            if (str.contains("papier")) {
+                nbPapier++;
+            }
+            if (str.contains("plastique")) {
+                nbPlastique++;
+            }
+            if (str.contains("métal")) {
+                nbMetal++;
+            }
+            if (str.contains("autre")) {
+                nbAutre++;
+            }
+        }
+        hashMap.put("verre",nbVerre);
+        hashMap.put("carton",nbCarton);
+        hashMap.put("papier",nbPapier);
+        hashMap.put("plastique",nbPlastique);
+        hashMap.put("metal",nbMetal);
+        hashMap.put("autre",nbAutre);
+
+        return hashMap;
+
+
+    }
+
+    static Diagram createDiagram(int type, HashMap hm) throws Throwable {
+        switch (type){
+            case Baton: return new diagramBaton(hm);
+            case Circulaire: return new diagramCirculaire(hm);
+            default: throw new Throwable("pas fait");
+        }
+    }
 }
